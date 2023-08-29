9938424836ad39db7c7ca982c8431b826626e201
tansu <tansusumeyye@gmail.com>
2023-08-29 14:29:20 +0300

    2. Hafta Hard Skill Challenge

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Lütfen bir kelime giriniz");
+        String word = scan.nextLine();
+        word = word.trim();
+        System.out.println(word.toUpperCase() );
+    }
+}
